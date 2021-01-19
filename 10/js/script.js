ffff1a50c2c44544fe8cce81062b5264dbd83ed3
faustabmnyh <faustaabimanyu@gmail.com>
2021-01-19 16:17:30 +0700

    add file

---
+++ 10/js/script.js
@@ -0,0 +1,28 @@
+//  ini kalau gapake jquery
+// const pScroll = document.querySelectorAll(".page-scroll");
+
+// pScroll.forEach(function(e){
+//     e.addEventListener("click",function(el){
+        
+//     });
+// })
+
+
+// ini kalau pake jquery,tapi katanya dibootsrap lima jquery diapus jadi usahakan
+// mulai mengurafi pemakaian ini
+// $(".page-scroll").on("click",function(e){
+//     // memanggil attribute
+//     const tujuan = $(this).attr("href");
+//     // menangkap elemen yang bersangkutan
+//     const elemenTujuan = $(tujuan); // dia mengambil satu elemen
+
+
+//     // pindahkan scroll , scrollTop itu jarak saat kita menscroll web kita
+//     // offset itu jarak keatas elemen
+//     // $("body").animate({
+//     //     scrollTop: elemenTujuan.offset().top - 50
+//     // });
+//     // $("body").scrollTop("700");  di versi baru ini gabisa mungkin ada syntak lain
+
+//     e.preventDefault();
+// })
